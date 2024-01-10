a39742d07edb79aff0b615cd3aa6b7c847cedf4c
Federico-I <threxos.48@gmail.com>
2024-01-10 11:56:38 +0000

    children-prop-Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,54 @@
+import { useState } from "react";
 
+const messages = ["Learn React", "Apply for Jobs", "Invest your New Income"];
 
-function App() {
+export default function App() {
+
+  const [step, setStep ] = useState (1);
+  const [isOpen, setIsOpen] = useState(true);
+  // const [nameTest, setNameTest] =useState();
+
+  function handlePrevious() {
+    if (step > 1) setStep((s) => s - 1);
+    // setStep(step - 1);
+  };
+
+  function handleNext() {
+    if (step < 3) { 
+      setStep((s) => s + 1);
+    // setStep(step + 1);
+    }
+    // setNameTest({name: "Fred"});
+  };
+
+  /* Two diffrent components will have different memory alocation even if the code logic is the same. Ex: if you render the smae component twice and make changes on one of them, the state of the one you changed won't affect the state of the other, even when the page is refreshed. */
+
   return (
-    <div>
-      <h1>Hello Project</h1>
-    </div>
+    <>
+      <button className="close " onClick={() => setIsOpen((is) => !is)}>&times;</button>
+
+      { isOpen && (
+        <div className="steps">
+        <div className="numbers">
+          <div className={step >= 1 ? "active" : ""}>1</div>
+          <div className={step >= 2 ? "active" : ""}>2</div>
+          <div className={step >= 3 ? "active" : ""}>3</div>
+        </div>
+  
+        <p className="message">Step {step}: {messages[step-1]} {/*nameTest*/}</p>
+  
+        <div className="buttons">
+          <Button backgroundColor="#7950f2" textColor="#fff" onClick={handlePrevious}><span>{"<="}</span>Previous</Button>
+          <Button backgroundColor="#7950f2" textColor="#fff" onClick={handleNext}>Next<span>{"=>"}</span></Button>
+        </div>
+      </div>)
+      }
+    </>
   );
 }
 
-export default App;
+function Button (textColor, backgroundColor, onClick, children) {
+  return (
+    <button style={{backgroundColor: {backgroundColor}, color: {textColor}}} onClick={onClick}>{children}</button>
+  )
+}
